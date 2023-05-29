7f7b89f892ea3fceff1e8412c19ab0ff0c0f48b5
emonmahdi <developeremon1997@gmail.com>
2023-05-29 16:18:14 +0600

    [DONE] express mongoose, cors, parser setup

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from 'express' 
+import cors from 'cors';
+const app: Application = express()
+
+
+//middleware
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+// testing
+app.get('/', (req: Request, res: Response) => {
+  res.send('University Management Application')
+})
+
+
+
+export default app
